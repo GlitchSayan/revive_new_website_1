1298ffe36abe4f5a1035eb3b450961506b328bf1
Gaurav Kumar <gaurav1452001@gmail.com>
2025-08-30 18:58:24 +0530

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '@/components/homepage/header'
 import ReviveHelps from '@/components/homepage/revive_helps'
 import Questions from '@/components/homepage/questions'
+import Footer from '@/components/footer'
 const HomePage = () => {
   return (
     <div>
       <Header />
       <ReviveHelps />
       <Questions/>
+      <Footer />
     </div>
   )
 }
